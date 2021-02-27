c5347fd5e7ca65302341ef8eabe64c7caa0244e2
UrfaKebab2929 <66250626+UrfaKebab2929@users.noreply.github.com>
2021-02-27 01:58:14 -0800

    sad
    ads

---
+++ başlat.bat
@@ -0,0 +1,8 @@
+echo off
+title eyo
+color a
+cls
+:a
+node index.js
+goto a
+pause    
---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODE0ODY3MzY5NDUzMDI3MzQ5.YDkGYA.WpTGl3NqAiU1_y5Pv0bdnB4qr-U',
+        prefix: 'eyo ',
+        activity: 'Eyo',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
